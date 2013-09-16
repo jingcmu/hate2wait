9391899eb1ee4c42d4bb696a33580c83f986d39e
Jacky <jingcmu@qq.com>
2013-09-16 15:38:30 -0700

    new hate2wait

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hate2wait::Application.config.secret_token = '985a46965aaacdcbe1aa30fd88f5ed38ceb0e8c7853d32f7f327389f9dfc3c68d9f42c0c4c130ae50ad4fabd7c980da3ac27d1348f030713c32e6674f1274a52'
